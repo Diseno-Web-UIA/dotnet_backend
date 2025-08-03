6eb529aead9fad0d76fd350f1f9236ec62e28ce7
smukideejeah <smukideejeah@outlook.com>
2025-08-02 18:02:29 -0600

    Añadir controlador de usuarios con operaciones básicas
    Se ha creado la clase `UsersController` en el espacio de nombres `backend.Controllers`, que hereda de `ControllerBase`. Se han implementado métodos para las operaciones HTTP GET, POST, PUT y DELETE, incluyendo la recuperación de todos los clientes y la búsqueda de un cliente por ID, con manejo de excepciones. Se han añadido las directivas `using` necesarias y se ha inicializado el contexto de base de datos `lastprojectContext`.

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(lastprojectContext context) : ControllerBase
+    {
+        private readonly lastprojectContext _context = context;
+
+        // GET: api/<UsersController>
+        [HttpGet]
+        public async Task<IEnumerable<Persona>> Get()
+        {
+            var clients = _context.Persona.ToList();
+            return clients;
+        }
+
+        // GET api/<UsersController>/5
+        [HttpGet("{id}")]
+        public Persona Get(int id)
+        {
+            try
+            {
+                var client = _context.Persona.FirstOrDefault(c => c.idPersona == id);
+                return client;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception (ex) here if needed
+                return null; // or handle it as appropriate
+            }
+        }
+
+        // POST api/<UsersController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<UsersController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UsersController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
